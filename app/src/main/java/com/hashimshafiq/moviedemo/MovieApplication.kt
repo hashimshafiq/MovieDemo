eb10b4613c81f531587edb551ce65ec521948574
Hashim Shafiq <hashim@yallaparking.com>
2021-02-04 15:30:45 +0500

    added DarkMode

--- app/src/main/java/com/hashimshafiq/moviedemo/MovieApplication.kt
+++ app/src/main/java/com/hashimshafiq/moviedemo/MovieApplication.kt
@@ -24,5 +24,10 @@ class MovieApplication : Application() {
         applicationComponent.inject(this)
     }
 
+    // Needed to replace the component with a test specific one
+    fun setComponent(applicationComponent: ApplicationComponent) {
+        this.applicationComponent = applicationComponent
+    }
+
 
 }
